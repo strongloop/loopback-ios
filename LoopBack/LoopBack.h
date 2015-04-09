9fefa988a5ec89bc15865593a79695e8609b250c
Sylvain Ageneau <ageneau@gmail.com>
2015-04-09 15:19:17 -0300

    Compile on MacOSX

--- LoopBack/LoopBack.h
+++ LoopBack/LoopBack.h
@@ -8,7 +8,9 @@
 #import "LBModel.h"
 #import "LBRESTAdapter.h"
 #import "LBInstallation.h"
+#if !defined(TARGET_OS_MAC)
 #import "LBPushNotification.h"
+#endif
 #import "LBUser.h"
 #import "LBFile.h"
 #import "LBContainer.h"
